34a8fc0009caa4b8f85c3bbf5aeeb3f25a03d10e
Robert Pedersen <rob@worldcrimeleague.org>
2019-07-30 11:19:12 -0400

    made civic card

--- src/CivicInfoContainer.js
+++ src/CivicInfoContainer.js
@@ -3,6 +3,8 @@ import CivicsCard from './CivicsCard.js'
 
 class CivicInfoContainer extends Component {
 
+
+
   constructor() {
     super()
 
@@ -12,14 +14,15 @@ class CivicInfoContainer extends Component {
 
   }
 
+
+
   componentDidMount() {
-    fetch("https://www.googleapis.com/civicinfo/v2/representatives?address=20902&includeOffices=true&levels=country&levels=regional&levels=locality&key=AIzaSyCVzfByi-RjV-MHC_xMXztyvelmIjB9RMA")
+    fetch(`https://www.googleapis.com/civicinfo/v2/representatives?address=20855&includeOffices=true&levels=country&levels=regional&levels=locality&key=${process.env.REACT_APP_GOOGLE_CIVIC_API}`)
       .then(resp => resp.json())
       .then(data => this.cardData(data))
   }
 
   cardData = (data) =>  {
-
     let x = 0
     let l = data.officials.length - 1
     let civicsdata = []
@@ -49,33 +52,13 @@ class CivicInfoContainer extends Component {
   render() {
     return (
       <div class="row">
-        <div class="col-sm-2">
-          <CivicsCard />
-        </div>
+          {this.state.civicsdata.map(polObj => <CivicsCard
+            polObj={polObj}
+            key={polObj.name}
+            />)}
       </div>
     )
   }
-
 }
 
 export default CivicInfoContainer
-
-// while(x <= l) {
-//     if (x === 3){
-//       pol.title = data.offices[2].name
-//       pol.name = data.officials[x].name
-//       pol.party = data.officials[x].party
-//       pol.phone = data.officials[x].phones
-//       pol.url = data.officials[x].urls[0]
-//       civicsdata.push(pol)
-//       x += 1
-//     }else{
-//       pol.title = data.offices[x].name
-//       pol.name = data.officials[x].name
-//       pol.party = data.officials[x].party
-//       pol.phone = data.officials[x].phones
-//       pol.url = data.officials[x].urls[0]
-//       civicsdata.push(pol)
-//       x += 1
-//     }
-//   }
--- src/CivicsCard.js
+++ src/CivicsCard.js
@@ -1,28 +1,22 @@
 import React from 'react'
 
-const CivicsCard = () => {
+const CivicsCard = (props) => {
   return (
+   <div class="col-sm-2">
     <div className="card mb-3">
-      <h3 className="card-header">Card header</h3>
+      <h3 className="card-header">{props.polObj.title}</h3>
       <div className="card-body">
-        <h5 className="card-title">Special title treatment</h5>
-        <h6 className="card-subtitle text-muted">Support card subtitle</h6>
-      </div>
-      <div className="card-body">
-        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <h5 className="card-title">{props.polObj.name}</h5>
+        <h6 className="card-subtitle text-muted">{props.polObj.party}</h6>
       </div>
       <ul className="list-group list-group-flush">
-        <li className="list-group-item">Cras justo odio</li>
-        <li className="list-group-item">Dapibus ac facilisis in</li>
-        <li className="list-group-item">Vestibulum at eros</li>
+        <li className="list-group-item">Phone: {props.polObj.phone}</li>
       </ul>
       <div className="card-body">
-        <a href="#" className="card-link">Twitter</a>
-        <a href="#" className="card-link">Facebook</a>
-        <a href="#" className="card-link">Web</a>
+        <a href={props.polObj.url} className="card-link">Web</a>
       </div>
     </div>
-
+   </div>
   )
 }
 
